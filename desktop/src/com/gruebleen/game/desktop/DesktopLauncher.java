bdc9159dfc4af9b59a513560141ffa06a806fc40
Vitaliy Moskalenko <to-@mail.ru>
2018-12-18 18:16:07 +0400

    Initial commit

---
+++ desktop/src/com/gruebleen/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.gruebleen.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.gruebleen.game.GruebleenGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GruebleenGame(), config);
+	}
+}
